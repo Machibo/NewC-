092476e41430a52afef41fd701b12671bb140fef
Илья <machibosanofgod@gmail.com>
2022-12-18 22:36:10 +0300

    обновил решение задачи с двумя числами

--- DomZad/Program.cs
+++ DomZad/Program.cs
@@ -3,11 +3,20 @@
 //a=2;b=10;max=10
 //a=-9;b=-3;max=-3
 
-Console.WriteLine("Введите свое первое число");
+Console.WriteLine("Сейчас мы с вами сравним два числа");
+Console.WriteLine("Введите пожалуйста первое число");
 int a = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите свое второе число");
+Console.WriteLine("Введите пожалуйста второе число");
 int b = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите свое третье число");
-int a = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine($"Большее из этих двух чисел - {a}");
+
+}
+
+else if (a<b)
+{
+        Console.WriteLine($"Большее из этих двух чисел - {b}");
+
+}

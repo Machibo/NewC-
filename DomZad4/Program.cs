6d72c0d3a3bada5f0975fa4ecca58ca10b3e6b07
Илья <machibosanofgod@gmail.com>
2022-12-18 23:33:36 +0300

    решение задачи с выводом четных чисел

---
+++ DomZad4/Program.cs
@@ -0,0 +1,14 @@
+﻿// 
+Console.WriteLine("Выведем четные числа от 1 до Вашего числа");
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int N = 2;
+if ((n > 1))
+{
+    while (N <= n)
+    {
+        Console.WriteLine(N + "");
+        N = N + 2;
+    }
+}
+

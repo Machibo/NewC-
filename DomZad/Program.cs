fbce80832a5ad3aa1e8a7a73e766637944c62748
Илья <machibosanofgod@gmail.com>
2022-12-18 18:23:25 +0300

    решение

--- DomZad/Program.cs
+++ DomZad/Program.cs
@@ -2,22 +2,12 @@
 // a=5;b=7;max=7
 //a=2;b=10;max=10
 //a=-9;b=-3;max=-3
- 
+
 Console.WriteLine("Введите свое первое число");
 int a = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите свое второе число");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a < b)
-{
-    Console.WriteLine($" Максимальное число = {b}");
-}
-else if (a > b)
-{
-    Console.WriteLine($"Максимальное число = {a}");
 
-}
-else if (a == b)
-{
-    Console.WriteLine($"Числа: {a} и {b} равны");
-
-}
+Console.WriteLine("Введите свое третье число");
+int a = Convert.ToInt32(Console.ReadLine());
---
+++ DomZad1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите свое первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите свое второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите свое третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = Convert.ToInt32(Console.ReadLine());
+
+if (a<b)<c;

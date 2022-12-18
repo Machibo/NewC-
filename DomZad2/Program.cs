54ab13088f7d2c01e416cc3b7791e22027ff0fe1
Илья <machibosanofgod@gmail.com>
2022-12-18 18:21:05 +0300

    Решил задачу на нечетность

---
+++ DomZad2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу ,где 
+Console.WriteLine("Введите ваше число,а мы определим четное оно или нет");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%2==0)
+{
+ Console.WriteLine("Ваше число является четным");
+}
+else if (a%2>0)
+{
+    Console.WriteLine("Ваше число не является четным"); 
+}

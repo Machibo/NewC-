64e64cbd0e813d938c60fbf645b0345811c496ad
Илья <machibosanofgod@gmail.com>
2022-12-18 22:24:15 +0300

    решил задачу с тремя числами

---
+++ Domzad3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу,которая принимает на вход три числа 
+// и выдает максимальное из этих трех чисел.
+//2,3,7->7;
+//44,5,78->78;
+//22,3,9->22;
+
+int max;
+
+Console.WriteLine("Нужно ввести три ваших числа через пробел и мы определим какое из них больше.");
+Console.WriteLine("Первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+max=a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+Console.WriteLine($"Большее из трех чисел = {c}");
+}
+else
+{
+    Console.WriteLine($"Большее из трех чисел = {max}");
+}
+

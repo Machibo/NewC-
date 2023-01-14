e94b37073b6271908d684265051d606963acfaeb
Илья <machibosanofgod@gmail.com>
2023-01-14 13:09:42 +0300

    решение задачи

---
+++ zad24/Program.cs
@@ -0,0 +1,20 @@
+﻿// 
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers= SumNumbers(number);
+Console.WriteLine($"{sumNumbers}");
+
+int SumNumbers(int num)
+
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+
+}
+
+

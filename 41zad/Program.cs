ba0eb6a74b1ca12bfba65ffde37e798b1332d5bf
Илья <machibosanofgod@gmail.com>
2023-01-31 10:16:04 +0300

    решение 2

--- 41zad/Program.cs
+++ 41zad/Program.cs
@@ -1,34 +1,52 @@
-﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// // Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-int[] Arr = inputArr("Введите число:", "Ошибка ввода");
-int res = result(Arr);
-Console.WriteLine($"{String.Join(",", Arr)} --> {res}");
-int[] inputArr(string mesage, string error)
 
+// int[] Arr = inputArr("Введите число:", "Ошибка ввода");
+// int res = result(Arr);
+// Console.WriteLine($"{String.Join(",", Arr)} --> {res}");
+// int[] inputArr(string mesage, string error)
+
+// {
+// int[] arr = new int[8];
+// for (int i = 0; i < arr.Length; i++)
+// {
+// Console.Write($"{mesage} {i} из {arr.Length}:");
+// bool isCorrect = int.TryParse(Console.ReadLine(), out int n);
+// arr[i] = n;
+// if (!isCorrect)
+// {
+//  Console.WriteLine(error);
+// break;
+// };
+// }
+// return arr;
+// }
+// int result(int[] array)
+// {
+// int result = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+// if (array[i] > 0)
+// {
+// result++;
+// }
+// }
+// return result;
+// }
+
+
+                                                    // Вариант 2
+
+using System;
+static void Main()
 {
-int[] arr = new int[8];
-for (int i = 0; i < arr.Length; i++)
-{
-Console.Write($"{mesage} {i} из {arr.Length}:");
-bool isCorrect = int.TryParse(Console.ReadLine(), out int n);
-arr[i] = n;
-if (!isCorrect)
-{
- Console.WriteLine(error);
-break;
-};
+    Console.Write("Введи элементы (через пробел):");
+    int[] massiv= Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+    int count=0;
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        count++;
+    }
 }
-return arr;
-}
-int result(int[] array)
-{
-int result = 0;
-for (int i = 0; i < array.Length; i++)
-{
-if (array[i] > 0)
-{
-result++;
-}
-}
-return result;
-}
+Console.WriteLine($"Чисел больше нуля:{count}");
+

dc6287b5eb37e4568d1553f15784e2593969708f
Илья <machibosanofgod@gmail.com>
2023-02-07 11:26:09 +0300

    Compleate

---
+++ 54zad/Program.cs
@@ -0,0 +1,100 @@
+﻿// // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int columns = 0;
+int rows = 0;
+inputSizeArray(ref rows, ref columns);
+int[,] array0 = randomFillArray(columns, rows, 0, 10);
+printArray(array0);
+int[,] array1 = sortRows(array0);
+printSortRows(array1);
+
+void inputSizeArray(ref int a, ref int b)
+{
+    try
+    {
+
+        Console.Write("Введите колличество столбцов: ");
+        a = int.Parse(Console.ReadLine() ?? "");
+        Console.Write("Введите колличество строк: ");
+        b = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception ex)
+    {
+        Console.Write("Не верный формат", ex);
+    }
+}
+
+int[,] randomFillArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return arr;
+
+}
+
+int[,] sortRows(int[,] array)
+{
+    int[,] arr = array;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                    arr[i, k] = arr[i, k];
+                }
+            }
+        }
+
+    }
+    return arr;
+}
+
+void printArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($" {arr[i, j]}");
+        }
+        Console.WriteLine("");
+    }
+    Console.WriteLine("");
+}
+
+void printSortRows(int[,] arr)
+
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($" {arr[i, j]}");
+        }
+        Console.WriteLine("");
+    }
+
+    Console.WriteLine("");
+}

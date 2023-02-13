6affb89048508bfef2ad984781ed90ff11807c91
Илья <machibosanofgod@gmail.com>
2023-02-13 12:41:20 +0300

    ok

---
+++ 66zad/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу
+// которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int M = 0;
+int N = 0;
+inputNumber(ref M, ref N);
+int sum = recursion(M, N);
+Console.Write($"M = {M}; N = {N} --> {sum} ");
+void inputNumber(ref int a, ref int b)
+{
+    try
+    {
+        Console.Write("Введите число M: ");
+        a = int.Parse(Console.ReadLine() ?? "");
+        Console.Write("Введите число N: ");
+        b = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception ex)
+    {
+        Console.Write("Не верный формат", ex);
+    }
+}
+int recursion(int M, int N)
+{
+    if (M == N)
+    {
+        return N;
+    }
+    M = M + recursion(M + 1, N);
+    return M;
+}
